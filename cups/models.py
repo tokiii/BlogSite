ca5b672c74136ff015856cd97d22713e15056b5b
wuchanghe <499347533@qq.com>
2018-08-28 21:14:01 +0800

    Initial commit

---
+++ cups/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class BlogType(models.Model):
+    type_name = models.CharField(max_length=15,verbose_name="标题")
+
+    def __str__(self):
+        return self.type_name
+
+
+# verbose_name 后台可以显示中文标题
+class Blog(models.Model):
+    title = models.CharField(max_length=32, verbose_name="博客标题")
+    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING, verbose_name="博客类型")
+    content = models.TextField(verbose_name="博客内容")
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="博客作者")
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    last_update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    def __str__(self):
+        return "<Blog %s>" % self.title
